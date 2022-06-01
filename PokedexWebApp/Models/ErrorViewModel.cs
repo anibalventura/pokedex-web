21336c5767af048f83428b664828790eeed4a185
Anibal Ventura <anibalventura@icloud.com>
2022-05-31 20:20:08 -0400

    First commit.

---
+++ PokedexWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PokedexWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
+
